0d637ca18a368b04a1b3df21f485443fa4b7e64a
David Kennedy <david.kennedy@rotorvideos.com>
2019-09-15 22:39:34 +0100

    Drafting!

--- entrypoint.rb
+++ entrypoint.rb
@@ -1,5 +1,15 @@
-#!/usr/bin/env ruby
+require 'yaml'
+
 puts "ARGV is:"
 puts ARGV
 puts "Environment is:"
 puts ENV.keys
+channel = ARGV[1] || 'releases'
+event = ENV.fetch('GITHUB_EVENT_PATH')
+puts "CHANNEL is #{channel}"
+puts "EVENT is #{event}"
+
+
+parsed = YAML.load(File.open(event))
+puts "YAML IS:"
+puts parsed
