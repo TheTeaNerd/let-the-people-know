520013d60aa2e3a1d7de41030d9788fa29b065b1
David Kennedy <david.kennedy@rotorvideos.com>
2019-09-15 23:02:59 +0100

    Explore merges

--- entrypoint.rb
+++ entrypoint.rb
@@ -3,7 +3,7 @@ require 'yaml'
 puts "ARGV is:"
 puts ARGV
 puts "Environment is:"
-puts ENV.keys
+puts ENV
 channel = ARGV[1] || 'releases'
 event = ENV.fetch('GITHUB_EVENT_PATH')
 puts "CHANNEL is #{channel}"
@@ -17,7 +17,11 @@ puts parsed
 puts "Repository is '#{parsed.dig('repository', 'name')}"
 commits = parsed.fetch('commits')
 commits.each.with_index(1) do |commit, index|
-  puts "#{index}. #{commit.fetch('message')} (_#{commit.dig('author', 'name')}_)"
+  if commit.fetch('distinct') == 'false'
+    puts "#{index}. #{commit.fetch('message')} (_#{commit.dig('author', 'name')}_)"
+  else
+    puts "THIS IS A MERGE: #{commit.fetch('message')}"
+  end
 end
 
 puts 'Done'
