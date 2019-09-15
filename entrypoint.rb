536c4dfd87c3ac6f3054835b41ee6c3041b9ef20
David Kennedy <david.kennedy@rotorvideos.com>
2019-09-15 23:13:55 +0100

    Added debug

--- entrypoint.rb
+++ entrypoint.rb
@@ -16,7 +16,13 @@ puts parsed
 
 puts "Repository is '#{parsed.dig('repository', 'name')}"
 commits = parsed.fetch('commits')
+puts "Commits are:"
+
+puts commits
+
+puts
 commits.each.with_index(1) do |commit, index|
+  puts commit
   if commit.fetch('distinct') == 'false'
     puts "#{index}. #{commit.fetch('message')} (_#{commit.dig('author', 'name')}_)"
   else
