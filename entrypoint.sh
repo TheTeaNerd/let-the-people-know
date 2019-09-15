1072a17b26452c25c124276eba2ce0feef768b0b
David Kennedy <david.kennedy@rotorvideos.com>
2019-09-15 19:50:55 +0100

    Extract commits

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,3 +7,10 @@ env
 cat ${GITHUB_EVENT_PATH}
 echo "Done"
 
+cat ${GITHUB_EVENT_PATH} | jq -r '.commits[].message,.commits[].author.name' |  (
+    while read line; do
+        read message
+        read author
+        echo "${message} (_${author}_)"
+    done
+)
