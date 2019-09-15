8703bdec28747af56e2952c0b64f28c665816a6c
David Kennedy <david.kennedy@rotorvideos.com>
2019-09-15 22:52:39 +0100

    Drafting commits

--- entrypoint.rb
+++ entrypoint.rb
@@ -13,3 +13,11 @@ puts "EVENT is #{event}"
 parsed = YAML.load(File.open(event))
 puts "YAML IS:"
 puts parsed
+
+puts "Repository is '#{parsed.dig('repository', 'name')}"
+commits = parsed.fetch('commits')
+commits.each.with_index(1) do |commit, index|
+  puts "#{index}. #{commit.fetch('message')} (_#{commit.dig('author', 'name')}_)"
+end
+
+puts 'Done'
