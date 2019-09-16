bcf5978cd8a8c1fa4ab02dfbda06a298250f63e8
David Kennedy <david.kennedy@rotorvideos.com>
2019-09-16 11:15:51 +0100

    Add Slack notifier

--- entrypoint.rb
+++ entrypoint.rb
@@ -8,11 +8,11 @@ puts '------------------------'
 puts
 
 channel = ENV.fetch('SLACK_CHANNEL')
-puts 'Channel is #{channel}'
+puts "Channel is #{channel}"
 puts
 
 webhook = ENV.fetch('SLACK_WEBHOOK')
-puts 'Channel is #{webhook}'
+puts "Channel is #{webhook}"
 puts
 
 event = ENV.fetch('GITHUB_EVENT_PATH')
@@ -24,21 +24,26 @@ puts "Repository is '#{parsed.dig('repository', 'name')}'"
 puts
 commits = parsed.fetch('commits')
 puts "Commits are:"
-commits.select do |commit|
-  puts commit
+simple_commits, merge_commits = commits.partition do |commit|
   commit.fetch('distinct') == false
-end.each.with_index(1) do |commit, index|
+end
+
+simple_commits.each.with_index(1) do |commit, index|
   puts "#{index}. #{commit.fetch('message')} (_#{commit.dig('author', 'name')}_)"
 end
 puts
 puts
 
-commits.select do |commit|
-  puts commit
-  commit.fetch('distinct') == true
-end.each.with_index(1) do |commit, index|
+merge_commits.each.with_index(1) do |commit, index|
   puts "Merge #{index}. #{commit.fetch('message')} (_#{commit.dig('author', 'name')}_)"
 end
+
+
+notifier = Slack::Notifier.new(webhook) do
+  defaults channel: "##{channel}"
+           username: "Let the people know"
+end
+notifier.ping "Hello default"
 
 puts
 puts 'Done'
