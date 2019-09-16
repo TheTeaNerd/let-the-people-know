32468f14de7d5c2430c60c44f29ad0f8baf77675
David Kennedy <david.kennedy@rotorvideos.com>
2019-09-16 09:39:31 +0100

    Debug

--- entrypoint.rb
+++ entrypoint.rb
@@ -1,6 +1,10 @@
 require 'yaml'
 
+
+puts 'ENVIRONMENT:'
 puts ENV
+puts
+
 channel = ARGV[1] || 'releases'
 event = ENV.fetch('GITHUB_EVENT_PATH')
 puts "EVENT is #{event}"
@@ -12,7 +16,17 @@ puts "Repository is '#{parsed.dig('repository', 'name')}'"
 commits = parsed.fetch('commits')
 puts "Commits are:"
 commits.select do |commit|
+  puts commit
   commit.fetch('distinct') == false
 end.each.with_index(1) do |commit, index|
   puts "#{index}. #{commit.fetch('message')} (_#{commit.dig('author', 'name')}_)"
+end
+puts
+puts
+
+commits.select do |commit|
+  puts commit
+  commit.fetch('distinct') == true
+end.each.with_index(1) do |commit, index|
+  puts "Merge #{index}. #{commit.fetch('message')} (_#{commit.dig('author', 'name')}_)"
 end
