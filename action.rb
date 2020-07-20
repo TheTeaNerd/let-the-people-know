7906a53623ffbd2433913da34ea22b780cc9d22b
David Kennedy <david.kennedy@rotorvideos.com>
2020-07-20 11:53:28 +0100

    🐛  Fix broken formatting

--- action.rb
+++ action.rb
@@ -42,14 +42,14 @@ commits.each do |commit|
     summary = message_lines.first.sub(/Bumps? /, 'Upgrades library ')
     announcement += "• #{summary} _(:robot_face: Dependabot)_\n"
 
-    release_notes = message_lines.select { |line| line.match?('Release notes') }
+    release_notes = message_lines.select { |line| line.match?('Release notes') }.first
     if release_notes
-      announcement += "> #{release_notes.gsub('["-','').gsub('"]','')}\n"
+      announcement += "> #{release_notes}\n"
     end
 
-    change_log = message_lines.select { |line| line.match?('Changelog') }
+    change_log = message_lines.select { |line| line.match?('Changelog') }.first
     if change_log
-      announcement += "> #{change_log.gsub('["-','').gsub('"]','')}\n"
+      announcement += "> #{change_log}\n"
     end
   else
     summary.gsub!(/#(\d+)/, "[#\\1](#{repository_url}/issues/\\1)")
